d75b9848c604e5c4dec05c1cbd59673c1335c3c9
mvpbarbosa <97544652+mvpbarbosa@users.noreply.github.com>
2022-05-25 19:17:49 -0300

    Create

--- assets/script.js
+++ assets/script.js
@@ -11,10 +11,10 @@ async function findAllPosts() {
       `<div class="PostListItem">
           <div class="PostListItem__Header">
             <div class="ProfileInfo">
-              <img src="${post.photo}" alt="Imagem de perfil" />
+              <img src="${post.photo}" alt="Profile picture" />
               <div class="NameDate">
                 <h3>${post.name}</h3>
-                <h4>${post.dateHour}</h4>
+                <h4 id="dateHour">${post.dateHour}</h4>
               </div>
             </div>
 
@@ -28,7 +28,7 @@ async function findAllPosts() {
           </div>
 
           <div class="PostListItem__Footer">
-            <img src="https://icones.pro/wp-content/uploads/2021/04/icone-noire-noir.png" alt="Like" />
+            <img src="./assets/images/like-icon.png" alt="Like button" />
           </div>
         </div>`
     );
@@ -44,16 +44,14 @@ const findByIdPost = async () => {
 
   const chosenPostDiv = document.querySelector("#OpenModal");
 
-  console.log("Funcionando")
-
   chosenPostDiv.innerHTML = `
   <div class="ModalItem">
   <div class="ModalItem__Header">
     <div class="ProfileInfo__Modal">
-      <img src="${post.photo}" alt="Imagem de perfil" />
+      <img src="${post.photo}" alt="Profile picture" />
       <div class="NameDate__Modal">
         <h3>${post.name}</h3>
-        <h4>${post.dateHour}</h4>
+        <h4 id="dateHour">${post.dateHour}</h4>
       </div>
     </div>
   </div>
@@ -65,22 +63,109 @@ const findByIdPost = async () => {
   </div>
 
   <div class="ModalItem__Footer">
-    <img src="https://icones.pro/wp-content/uploads/2021/04/icone-noire-noir.png" alt="Like" />
+    <img src="./assets/images/like-icon.png" alt="Like button" />
   </div>
   </div>`;
-
 };
 
+async function createPost() {
+  // Função para formatar 1 em 01
+  const zeroFill = (n) => {
+    return ("0" + n).slice(-2);
+  };
+  // Pega o horário atual
+  const now = new Date();
+
+  const monName = new Array(
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "agosto",
+    "outubro",
+    "novembro",
+    "dezembro"
+  );
+
+  // Formata a data conforme dd/mm/aaaa hh:ii:ss
+  const dateHour =
+    now.getDate() +
+    " de " +
+    monName[now.getMonth()] +
+    " de " +
+    now.getFullYear() +
+    " às " +
+    zeroFill(now.getHours()) +
+    ":" +
+    zeroFill(now.getMinutes());
+
+  const photo = document.querySelector("#photo").value;
+  const name = document.querySelector("#name").value;
+  const text = document.querySelector("#text").value;
+
+  const post = {
+    photo,
+    name,
+    dateHour,
+    text,
+  };
+
+  const response = await fetch(`${baseUrl}/create-post`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(post),
+  });
+
+  const newPost = await response.json();
+
+  const html = `<div class="PostListItem">
+          <div class="PostListItem__Header">
+            <div class="ProfileInfo">
+              <img src="${newPost.photo}" alt="Profile picture" />
+              <div class="NameDate">
+                <h3>${newPost.name}</h3>
+                <h4 id="dateHour">${newPost.dateHour}</h4>
+              </div>
+            </div>
+
+            <img class="Options" src="./assets/images/more-options.png" alt="Options" />
+          </div>
+
+          <div class="PostListItem__Main">
+            <span>
+              ${newPost.text}
+            </span>
+          </div>
+
+          <div class="PostListItem__Footer">
+            <img src="./assets/images/like-icon.png" alt="Like button" />
+          </div>
+        </div>`;
+
+  document.querySelector(".PostList").insertAdjacentHTML("afterbegin", html);
+
+  closeModal();
+}
+
 function openModal() {
-  document.querySelector(".ModalOverlay").style.display = "flex"
+  document.querySelector(".ModalOverlay").style.display = "flex";
 }
 
 function closeModal() {
-  document.querySelector(".ModalOverlay").style.display = "none"
+  document.querySelector(".ModalOverlay").style.display = "none";
 }
 
-// function abrirModalDelete(id) {
-//   document.querySelector("#overlay-delete").style.display = "flex";
-// }
+function openCreateModal() {
+  document.querySelector(".CreateModalOverlay").style.display = "flex";
+}
+
+function closeCreateModal() {
+  document.querySelector(".CreateModalOverlay").style.display = "none";
+}
 
 findAllPosts();
